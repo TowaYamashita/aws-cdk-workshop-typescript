2e188439359e29d5c21d4a337c2b58b6401edf8b
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-11-04 23:07:52 +0900

    変更: CDKプロジェクトにHitCounterを追加

--- cdk-workshop/lambda/hello.mjs
+++ cdk-workshop/lambda/hello.mjs
@@ -1,4 +1,4 @@
-exports.handler = async function(event) {
+export const handler =  async function(event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
---
+++ cdk-workshop/lib/hitcounter.ts
@@ -0,0 +1,35 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+export interface HitCounterProps {
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+  public readonly handler: lambda.Function;
+
+  constructor(scope: Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, 'Hits', {
+        partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'hitcounter.handler',
+        code: lambda.Code.fromAsset('lambda'),
+        environment: {
+            DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+            HITS_TABLE_NAME: table.tableName
+        }
+    });
+
+    // DynamoDB の読み書きができる権限を付与
+    table.grantReadWriteData(this.handler);
+
+    // ダウンストリームのLambda関数を呼び出す権限を付与
+    props.downstream.grantInvoke(this.handler);
+  }
+}
