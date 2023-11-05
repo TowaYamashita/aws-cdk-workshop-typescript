83b1aabfdc5bd2fb16a6bb29d8a3f25c1ba8c99f
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-11-05 20:46:20 +0900

    変更: テストコードを追加

---
+++ cdk-workshop/test/hitcounter.test.ts
@@ -0,0 +1,70 @@
+import { Template, Capture } from 'aws-cdk-lib/assertions';
+import { Stack } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { HitCounter }  from '../lib/hitcounter';
+
+test('DynamoDB Table Created With Encryption', () => {
+  const stack = new Stack();
+  
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream:  new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+
+  // DynamoDB テーブルが暗号化されているかどうか確認する
+  const template = Template.fromStack(stack);
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    SSESpecification: {
+      SSEEnabled: true
+    }
+  });
+});
+
+test('Lambda Has Environment Variables', () => {
+  const stack = new Stack();
+
+  new HitCounter(stack, 'MyTestConstruct', {
+    downstream: new lambda.Function(stack, 'TestFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'hello.handler',
+      code: lambda.Code.fromAsset('lambda')
+    })
+  });
+
+  const template = Template.fromStack(stack);
+  const envCapture = new Capture();
+  template.hasResourceProperties("AWS::Lambda::Function", {
+    Environment: envCapture,
+  });
+
+  expect(envCapture.asObject()).toEqual(
+    {
+      Variables: {
+        DOWNSTREAM_FUNCTION_NAME: {
+          Ref: "TestFunction22AD90FC",
+        },
+        HITS_TABLE_NAME: {
+          Ref: "MyTestConstructHits24A357F0",
+        }
+      }
+    }
+  );
+});
+
+test('read capacity can be configured', () => {
+  const stack = new Stack();
+
+  expect(() => {
+    new HitCounter(stack, 'MyTestConstruct', {
+      downstream: new lambda.Function(stack, 'TestFunction', {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'hello.handler',
+        code: lambda.Code.fromAsset('lambda')
+      }),
+      readCapacity: 3
+    });
+  }).toThrow(/readCapacity must be greater than 5 and less than 20/)
+});
